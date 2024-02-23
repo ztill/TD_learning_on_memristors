28dac920e846a72aa6b006085ac352c1008c9d4f
Till Zellweger <till.zellweger@iis.ee.ethz.ch>
2024-02-24 00:54:34 +0100

    Add T-Maze script

---
+++ grid_searches/_context.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, basedir)
+
+import src
--- scripts/_context.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, basedir)
-
-import src
